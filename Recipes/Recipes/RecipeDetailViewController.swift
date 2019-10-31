ae3e28d98dfcfff192dbc159d81e99e2f09abf84
Blake Price <blakeandrewprice@gmail.com>
2019-10-31 12:27:57 -0700

    Started Recipes! Part 1: Storyboard finished for now!

---
+++ Recipes/Recipes/RecipeDetailViewController.swift
@@ -0,0 +1,19 @@
+//
+//  RecipeDetailViewController.swift
+//  Recipes
+//
+//  Created by Blake Andrew Price on 10/31/19.
+//  Copyright © 2019 Lambda Inc. All rights reserved.
+//
+
+import UIKit
+
+class RecipeDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var textView: UITextView!
+    
+}
