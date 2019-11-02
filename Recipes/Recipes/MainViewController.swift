a5c85bbb866257c333f566765b3bc285ee61a0e5
Blake Price <blakeandrewprice@gmail.com>
2019-11-01 23:03:32 -0700

    Project is complete without stretch goals.

--- Recipes/Recipes/MainViewController.swift
+++ Recipes/Recipes/MainViewController.swift
@@ -12,10 +12,59 @@ class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        networkClient.fetchRecipes { (recipeArray, error) in
+            if error != nil {
+                NSLog(error.debugDescription)
+                return
+            } else {
+                return self.allRecipes = recipeArray!
+            }
+        }
     }
     
     @IBOutlet weak var mainViewTextField: UITextField!
     @IBAction func mainViewTextFieldAction(_ sender: Any) {
+        mainViewTextField.resignFirstResponder()
+        filterRecipes()
     }
     
+    let networkClient = RecipesNetworkClient()
+    var allRecipes: [Recipe] = [] {
+        didSet{
+            filterRecipes()
+        }
+    }
+    var recipesTableViewController: RecipesTableViewController? {
+        didSet{
+            recipesTableViewController?.recipes = filteredRecipes
+        }
+    }
+    
+    var filteredRecipes: [Recipe] = [] {
+        didSet{
+            recipesTableViewController?.recipes = filteredRecipes
+        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "EmbedTableViewSegue" {
+            recipesTableViewController = segue.destination as? RecipesTableViewController
+        }
+    }
+    
+    func filterRecipes() {
+        if let search = mainViewTextField.text {
+            if search == "" {
+                filteredRecipes = allRecipes
+            } else {
+                filteredRecipes = allRecipes.filter { (recipe) -> Bool in
+                    return recipe.name.contains(search) || recipe.instructions.contains(search)
+                }
+            }
+        } else {
+            filteredRecipes = allRecipes
+        }
+    }
 }
+    
+
